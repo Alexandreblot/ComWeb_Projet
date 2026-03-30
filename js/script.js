1b92baa2fa929fd41e77e1a5c71c62bcb59b6e9d
G-Asheeeesh <gabriel.pagniez-quiroga@isen-ouest.yncrea.fr>
2026-03-30 17:30:46 +0200

    script.js + index.html

--- js/script.js
+++ js/script.js
@@ -18,6 +18,36 @@ function requestProducts(category = null) {
         .then(data => displayProducts(data));
 }
 
+function displayProducts(products) {
+    const grid = document.getElementById('product-grid');
+    grid.innerHTML = ''; 
+
+    products.forEach(product => {
+        const col = document.createElement('div');
+        col.className = 'col-md-4'; 
+
+        col.innerHTML = `
+            <div class="card h-100">
+                <div class="card-body">
+                    <h5 class="card-title">${product.name}</h5>
+                    
+                    <img src="img/${product.id}.jpg" class="card-img-top" alt="${product.name}" 
+                         onerror="this.src='img/tachyon_idle.jpeg'">
+                    
+                    <p class="card-text text-truncate">${product.description}</p>
+                    <p class="fw-bold">${product.price} €</p>
+                    
+                    <button class="btn btn-sm btn-outline-primary" onclick="loadProductDetail(${product.id})">
+                        Voir détails
+                    </button>
+                </div>
+            </div>
+        `;
+        grid.appendChild(col);
+    });
+}
+
+
 function requestProduct(id) {
     fetch(API_BASE + 'product.php?id=' + id)
         .then(res => res.json())
